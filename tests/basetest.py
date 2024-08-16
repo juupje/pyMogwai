d67a3d6756e103f5ef277d918dec4406b7299498
Wolfgang Fahl <wf@bitplan.com>
2024-08-16 11:13:44 +0200

    fixes tests

--- tests/basetest.py
+++ tests/basetest.py
@@ -10,7 +10,8 @@ class BaseTest(Basetest):
 
     def setUp(self, debug=True, profile=True):
         Basetest.setUp(self, debug=debug, profile=profile)
-        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.script_dir = os.path.dirname(__file__)
         self.documents_path = os.path.join(self.script_dir, "documents")
         self.examples_path = os.path.join(os.path.dirname(self.script_dir),"mogwai_examples")
-        self.root_path = self.script_dir
+        self.root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
